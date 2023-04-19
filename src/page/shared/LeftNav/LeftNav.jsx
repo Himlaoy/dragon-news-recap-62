51b04a1b48917812bf10ee24a1ac3d7f5a1ec61e
himaloyash <himaloyashis98@gmail.com>
2023-04-19 13:23:08 +0600

    I'm done

--- src/page/shared/LeftNav/LeftNav.jsx
+++ src/page/shared/LeftNav/LeftNav.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Collapse } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
     const [category, setCategory] = useState([])
     console.log(category)
 
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:5000/category')
-        .then(res=>res.json())
-        .then(data=>setCategory(data))
-        .catch(error=>console.log(error.message))
-    },[])
+            .then(res => res.json())
+            .then(data => setCategory(data))
+            .catch(error => console.log(error.message))
+    }, [])
 
     return (
         <div>
-            <h4>Left nav is coming</h4>
+            <h4>All category</h4>
+            <div>
+                {
+                    category.map(cate => <p cate={cate.id}>
+                        <Link to={`/${cate.id}`} className='text-decoration-none text-black'>{cate.name}</Link>
+                    </p> )
+                }
+            </div>
         </div>
     );
 };
